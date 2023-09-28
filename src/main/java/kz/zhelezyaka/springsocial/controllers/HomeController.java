4edadedea088bd7dd70adb1440900e7230a19045
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-09-28 09:38:59 +0300

    Added Home Controller

---
+++ src/main/java/kz/zhelezyaka/springsocial/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springsocial.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/index")
+    public String home(){
+        return "index";
+    }
+}
---
+++ src/test/java/kz/zhelezyaka/springsocial/SpringSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
